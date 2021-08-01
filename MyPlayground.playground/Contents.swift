3306e205eb6fc3f293607be0ef42041343ccc638
leavelet <leavelet0@gmail.com>
2021-08-01 11:20:37 +0800

    2021-08-01

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,58 @@
 print("hello world")
 
+var myVariable = 42
+myVariable = 50
+let myConstant = 20
 
+print(myVariable)
+
+print(myVariable,myConstant)
+
+let inplicitInt: Double = 70
+
+let lebel = "The lenth is "
+
+let width = 94
+
+let widthlebel = lebel + String(width)
+
+print(widthlebel)
+
+var st: String = "nmsl \(width)"
+
+print(st)
+
+var shoppingList = ["cat","dog","milk"]
+
+shoppingList[1] = "233"
+
+print(shoppingList)
+
+var occupations = [
+    "oppoo": "ma",
+    "bssc": "dd",
+    "basic": "mm",
+]
+
+print(occupations)
+
+
+
+var emptyDictiona = [String: Float]()
+
+var emptyArray = [String]()
+
+print(emptyArray)
+
+var optionalString:String? = "Hello"
+
+print(optionalString == nil)
+
+var optionalName: String? = nil
+var greeting = "Hello "
+if let name = optionalName {
+    greeting = "Hello, \(name)"
+    print(greeting)
+}else {
+    print("Invaild name! ")
+}
